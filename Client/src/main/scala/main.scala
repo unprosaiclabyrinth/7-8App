ebd50613fa3e8820cb64b3d9bec002f16154c7e7
unprosaiclabyrinth <hdongr2@uic.edu>
2025-05-15 17:52:34 -0500

    Use foreach instead of map

--- Client/src/main/scala/main.scala
+++ Client/src/main/scala/main.scala
@@ -4,7 +4,7 @@
 def main(): Unit =
   //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
   // to see how IntelliJ IDEA suggests fixing it.
-  (1 to 5).map(println)
+  (1 to 5).foreach(println)
 
   for (i <- 1 to 5) do
     //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
