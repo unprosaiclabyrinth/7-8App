a3b5e7e71a4fe9d8563e6e1324ab281996a6b1c5
unprosaiclabyrinth <hdongr2@uic.edu>
2025-05-17 04:26:41 -0500

    Add a message to transmit the trump

--- Server/src/main/scala/Message.scala
+++ Server/src/main/scala/Message.scala
@@ -2,6 +2,7 @@ sealed trait Message extends Serializable
 
 case object WaitingFor1ToStart extends Message
 case class CallingTrump(caller: Int) extends Message
+case class Trump(trump: Suit) extends Message
 case class Move(turn: Int, alreadyPlayed: Option[Card], bottomReveal: Option[Card]) extends Message
 case class RoundResult(roundWinner: Int) extends Message
 case class GameResult(winner: Int) extends Message
