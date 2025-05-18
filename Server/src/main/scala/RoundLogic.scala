df61def85bba4403d07250c1258033228371ad05
unprosaiclabyrinth <hdongr2@uic.edu>
2025-05-17 23:02:47 -0500

    Define logic per round

--- Server/src/main/scala/RoundLogic.scala
+++ Server/src/main/scala/RoundLogic.scala
@@ -1,4 +1,4 @@
-class GameLogic(top1: List[Card],
+class RoundLogic(top1: List[Card],
                 top2: List[Card],
                 bottomDown1: List[Card],
                 bottomDown2: List[Card],
@@ -6,13 +6,16 @@ class GameLogic(top1: List[Card],
                 bottomUp2: List[Card],
                 trump: Suit):
   require(
-    top1.length == 5 && top2.length == 5 && bottomDown1.length == 5 
+    top1.length == 5 && top2.length == 5 && bottomDown1.length == 5
       && bottomDown2.length == 5 && bottomUp1.length == 5 && bottomUp2.length == 5,
     "*** Invalid deal."
   )
-  
-  def isValidMove(player: Int, move: Card): Boolean =
+
+  def isValidMove(player: Int, oppMove: Option[Card]): Boolean =
     ???
-    
-  def roundWinner(card1: Card, card2: Card): 1|2 =
+
+  def roundWinner(card1: Card, card2: Card): 1 | 2 =
+    ???
+
+  def gameWinner: Option[1 | 2] =
     ???
