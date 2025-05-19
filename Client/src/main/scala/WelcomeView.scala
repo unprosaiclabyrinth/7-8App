a7001ac6b060b4e7eba4ca03c9f0e8123113c994
unprosaiclabyrinth <hdongr2@uic.edu>
2025-05-18 22:23:20 -0500

    Build an initial view

--- Client/src/main/scala/WelcomeView.scala
+++ Client/src/main/scala/WelcomeView.scala
@@ -3,37 +3,45 @@ import scalafx.application.JFXApp3.PrimaryStage
 import scalafx.scene.control.{Button, TextField}
 import scalafx.geometry.Pos
 import scalafx.scene.Scene
-import scalafx.scene.layout.VBox
+import scalafx.scene.layout.{HBox, VBox}
 
 object WelcomeView extends JFXApp3:
   override def start(): Unit =
     val hostField = new TextField {
       id = "hostField"
       promptText = "Server host"
-      disable = true
-      alignment = Pos.Center
+      focusTraversable = false
       editable = true
     }
     val portField = new TextField {
       id = "portField"
       promptText = "Server port"
-      disable = true
-      alignment = Pos.Center
+      focusTraversable = false
       editable = true
     }
-    val playButton = new Button {
-      id = "playButton"
-      text = "Play"
+    val playButton = new HBox {
+      id = "play"
       alignment = Pos.Center
+      children = Seq(
+        new Button {
+          id = "playButton"
+          text = "Play"
+          alignment = Pos.Center
+          prefHeight = 140
+          prefWidth = 140
+        }
+      )
+      prefHeight = 300
+      prefWidth = 300
     }
     val root = new VBox {
       style = "-fx-background-color: Black;"
       children = Seq(hostField, portField, playButton)
-      prefHeight = 75
-      prefWidth = 75
     }
 
     stage = new PrimaryStage {
       title = "Welcome"
-      scene = new Scene(root, 300, 150)
+      scene = new Scene(root, 400, 400) {
+        stylesheets.add(getClass.getResource("/css/welcome.css").toExternalForm)
+      }
     }
