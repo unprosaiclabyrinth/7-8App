2a6704bf5c0f653ee1aa70f7126b5b907e640397
unprosaiclabyrinth <hdongr2@uic.edu>
2025-07-13 13:09:19 -0500

    Modularize views, networking, and navigation

--- Client/src/main/scala/Player.scala
+++ Client/src/main/scala/Player.scala
@@ -1,1 +1,22 @@
-object Player
+import scalafx.application.JFXApp3
+import scalafx.application.JFXApp3.PrimaryStage
+import scalafx.scene.Scene
+
+import java.net.Socket
+import scala.util.Try
+
+object Player extends JFXApp3:
+  override def start(): Unit =
+    val welcomeRoot = WelcomeView(sock =>
+      println(s"Connected to ${sock.getInetAddress}:${sock.getPort}")
+    )
+
+    SceneNavigator.init(new PrimaryStage {
+      title = "Welcome"
+      scene = new Scene(welcomeRoot, 400, 400) {
+        stylesheets.add(getClass.getResource("/css/welcome.css").toExternalForm)
+      }
+    })
+
+  def connectTo(host: String, port: Int): Try[Socket] =
+    Try(new Socket(host, port))
--- Client/src/main/scala/WelcomeView.scala
+++ Client/src/main/scala/WelcomeView.scala
@@ -1,76 +1,64 @@
-import scalafx.application.JFXApp3
-import scalafx.application.JFXApp3.PrimaryStage
 import scalafx.geometry.Pos
-import scalafx.scene.Scene
 import scalafx.scene.control.{Button, TextField}
 import scalafx.scene.layout.{HBox, VBox}
+
 import java.net.Socket
+import scala.util.{Failure, Success}
 
-object WelcomeView extends JFXApp3:
-  override def start(): Unit =
-    val hostField = new TextField {
-      id = "hostField"
-      promptText = "Server host"
-      focusTraversable = false
-      editable = true
-    }
+object WelcomeView:
+  private val hostField = new TextField {
+    id = "hostField"
+    promptText = "Server host"
+    focusTraversable = false
+    editable = true
+  }
 
-    val portField = new TextField {
-      id = "portField"
-      promptText = "Server port"
-      focusTraversable = false
-      editable = true
-    }
+  private val portField = new TextField {
+    id = "portField"
+    promptText = "Server port"
+    focusTraversable = false
+    editable = true
+  }
 
-    val playBtn = new Button {
-      id = "playButton"
-      text = "Play"
-      alignment = Pos.Center
-      prefHeight = 140
-      prefWidth = 140
-    }
+  private val playBtn = new Button {
+    id = "playButton"
+    text = "Play"
+    alignment = Pos.Center
+    prefHeight = 140
+    prefWidth = 140
+  }
 
-    val errorField = new TextField {
-      id = "errorField"
-      editable = false
-      focusTraversable = false
-      visible = false
-      managed = false
-    }
+  private val errorField = new TextField {
+    id = "errorField"
+    editable = false
+    focusTraversable = false
+    visible = false
+    managed = false
+  }
 
+  private val playBox = new HBox {
+    id = "play"
+    alignment = Pos.Center
+    children = Seq(playBtn)
+    prefHeight = 300
+    prefWidth = 300
+  }
+
+  def apply(onConnect: Socket => Unit): VBox =
     // when clicked, try to connect
     playBtn.onAction = _ =>
       val host = hostField.text.value
       // parse port or default to -1 so socket will fail
       val port = scala.util.Try(portField.text.value.toInt).getOrElse(-1)
 
-      try {
-        val sock = new Socket(host, port)
-        println(s"Connected to $host:$port") // or do something with sock
-        sock.close()
-      } catch {
-        case e: Throwable =>
+      Player.connectTo(host, port) match
+        case Success(sock) => onConnect(sock)
+        case Failure(_) =>
           errorField.text = s"Connection failed."
           errorField.visible = true
           errorField.managed = true
-      }
 
-    val playBox = new HBox {
-      id = "play"
-      alignment = Pos.Center
-      children = Seq(playBtn)
-      prefHeight = 300
-      prefWidth = 300
-    }
-
-    val root = new VBox {
+    new VBox {
       style = "-fx-background-color: Black;"
       children = Seq(hostField, portField, playBox, errorField)
-    }
-
-    stage = new PrimaryStage {
-      title = "Welcome"
-      scene = new Scene(root, 400, 400) {
-        stylesheets.add(getClass.getResource("/css/welcome.css").toExternalForm)
-      }
     }
