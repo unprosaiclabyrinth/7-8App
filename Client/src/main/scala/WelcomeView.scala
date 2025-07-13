3e67d9d8c18d05c1c0c377e0e7890a15ba6a5fda
unprosaiclabyrinth <hdongr2@uic.edu>
2025-07-13 12:08:00 -0500

    Add listener to connect to server

--- Client/src/main/scala/WelcomeView.scala
+++ Client/src/main/scala/WelcomeView.scala
@@ -1,9 +1,10 @@
 import scalafx.application.JFXApp3
 import scalafx.application.JFXApp3.PrimaryStage
-import scalafx.scene.control.{Button, TextField}
 import scalafx.geometry.Pos
 import scalafx.scene.Scene
+import scalafx.scene.control.{Button, TextField}
 import scalafx.scene.layout.{HBox, VBox}
+import java.net.Socket
 
 object WelcomeView extends JFXApp3:
   override def start(): Unit =
@@ -13,30 +14,58 @@ object WelcomeView extends JFXApp3:
       focusTraversable = false
       editable = true
     }
+
     val portField = new TextField {
       id = "portField"
       promptText = "Server port"
       focusTraversable = false
       editable = true
     }
-    val playButton = new HBox {
+
+    val playBtn = new Button {
+      id = "playButton"
+      text = "Play"
+      alignment = Pos.Center
+      prefHeight = 140
+      prefWidth = 140
+    }
+
+    val errorField = new TextField {
+      id = "errorField"
+      editable = false
+      focusTraversable = false
+      visible = false
+      managed = false
+    }
+
+    // when clicked, try to connect
+    playBtn.onAction = _ =>
+      val host = hostField.text.value
+      // parse port or default to -1 so socket will fail
+      val port = scala.util.Try(portField.text.value.toInt).getOrElse(-1)
+
+      try {
+        val sock = new Socket(host, port)
+        println(s"Connected to $host:$port") // or do something with sock
+        sock.close()
+      } catch {
+        case e: Throwable =>
+          errorField.text = s"Connection failed."
+          errorField.visible = true
+          errorField.managed = true
+      }
+
+    val playBox = new HBox {
       id = "play"
       alignment = Pos.Center
-      children = Seq(
-        new Button {
-          id = "playButton"
-          text = "Play"
-          alignment = Pos.Center
-          prefHeight = 140
-          prefWidth = 140
-        }
-      )
+      children = Seq(playBtn)
       prefHeight = 300
       prefWidth = 300
     }
+
     val root = new VBox {
       style = "-fx-background-color: Black;"
-      children = Seq(hostField, portField, playButton)
+      children = Seq(hostField, portField, playBox, errorField)
     }
 
     stage = new PrimaryStage {
