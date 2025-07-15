35d898707ed86f75381dd6941266e1a5ea66b137
unprosaiclabyrinth <hdongr2@uic.edu>
2025-07-14 20:39:45 -0500

    Demodularize

--- Client/src/main/scala/Player.scala
+++ Client/src/main/scala/Player.scala
@@ -1,22 +1,25 @@
-import scalafx.application.JFXApp3
 import scalafx.application.JFXApp3.PrimaryStage
-import scalafx.scene.Scene
+import scalafx.scene.{Parent, Scene}
 
+import java.io.{ObjectInputStream, ObjectOutputStream}
 import java.net.Socket
+import scala.compiletime.uninitialized
 import scala.util.Try
 
-object Player extends JFXApp3:
-  override def start(): Unit =
-    val welcomeRoot = WelcomeView(sock =>
-      println(s"Connected to ${sock.getInetAddress}:${sock.getPort}")
-    )
-
-    SceneNavigator.init(new PrimaryStage {
-      title = "Welcome"
-      scene = new Scene(welcomeRoot, 400, 400) {
-        stylesheets.add(getClass.getResource("/css/welcome.css").toExternalForm)
-      }
-    })
+object Player:
+  var socket: Socket = uninitialized
+  var out: ObjectOutputStream = uninitialized
+  var in: ObjectInputStream = uninitialized
 
   def connectTo(host: String, port: Int): Try[Socket] =
     Try(new Socket(host, port))
+
+  def setStage(stageTitle: String,
+                       root: Parent, w: Double, h: Double,
+                       css: String): PrimaryStage =
+    new PrimaryStage {
+      title = stageTitle
+      scene = new Scene(root, w, h) {
+        stylesheets.add(getClass.getResource(css).toExternalForm)
+      }
+    }
--- Client/src/main/scala/WelcomeView.scala
+++ Client/src/main/scala/WelcomeView.scala
@@ -1,50 +1,56 @@
+import scalafx.application.JFXApp3
 import scalafx.geometry.Pos
 import scalafx.scene.control.{Button, TextField}
 import scalafx.scene.layout.{HBox, VBox}
 
-import java.net.Socket
+import java.io.{ObjectInputStream, ObjectOutputStream}
 import scala.util.{Failure, Success}
 
-object WelcomeView:
-  private val hostField = new TextField {
-    id = "hostField"
-    promptText = "Server host"
-    focusTraversable = false
-    editable = true
-  }
+object WelcomeView extends JFXApp3:
+  override def start(): Unit =
+    val hostField = new TextField {
+      id = "hostField"
+      promptText = "Server host"
+      focusTraversable = false
+      editable = true
+    }
 
-  private val portField = new TextField {
-    id = "portField"
-    promptText = "Server port"
-    focusTraversable = false
-    editable = true
-  }
+    val portField = new TextField {
+      id = "portField"
+      promptText = "Server port"
+      focusTraversable = false
+      editable = true
+    }
 
-  private val playBtn = new Button {
-    id = "playButton"
-    text = "Play"
-    alignment = Pos.Center
-    prefHeight = 140
-    prefWidth = 140
-  }
+    val playBtn = new Button {
+      id = "playButton"
+      text = "Play"
+      alignment = Pos.Center
+      prefHeight = 140
+      prefWidth = 140
+    }
 
-  private val errorField = new TextField {
-    id = "errorField"
-    editable = false
-    focusTraversable = false
-    visible = false
-    managed = false
-  }
+     val errorField = new TextField {
+      id = "errorField"
+      editable = false
+      focusTraversable = false
+      visible = false
+      managed = false
+    }
 
-  private val playBox = new HBox {
-    id = "play"
-    alignment = Pos.Center
-    children = Seq(playBtn)
-    prefHeight = 300
-    prefWidth = 300
-  }
+     val playBox = new HBox {
+      id = "play"
+      alignment = Pos.Center
+      children = Seq(playBtn)
+      prefHeight = 300
+      prefWidth = 300
+    }
 
-  def apply(onConnect: Socket => Unit): VBox =
+    val root = new VBox {
+      style = "-fx-background-color: Black;"
+      children = Seq(hostField, portField, playBox, errorField)
+    }
+
     // when clicked, try to connect
     playBtn.onAction = _ =>
       val host = hostField.text.value
@@ -52,13 +58,14 @@ object WelcomeView:
       val port = scala.util.Try(portField.text.value.toInt).getOrElse(-1)
 
       Player.connectTo(host, port) match
-        case Success(sock) => onConnect(sock)
+        case Success(sock) =>
+          println(s"Connected to ${sock.getInetAddress}:${sock.getPort}")
+          Player.socket = sock
+          Player.out = ObjectOutputStream(sock.getOutputStream)
+          Player.in = ObjectInputStream(sock.getInputStream)
         case Failure(_) =>
           errorField.text = s"Connection failed."
           errorField.visible = true
           errorField.managed = true
-
-    new VBox {
-      style = "-fx-background-color: Black;"
-      children = Seq(hostField, portField, playBox, errorField)
-    }
+    
+    stage = Player.setStage("Welcome", root, 400, 400, "/css/welcome.css")
